96544e897e44bd5058ee03db768d8c75edb7330b
Gianlucaorreale <gianlucacorreale.1996@gmail.com>
2022-06-24 09:48:38 +0200

    fixed-exercise

--- js/script.js
+++ js/script.js
@@ -11,20 +11,19 @@ Consigli del giorno:
 
 
 for(let i=1; i<=100; i++){
-    console.log(i);
-     const multipleThree = i % 3;
-     const multipleFive = i % 5;
+  
 
-     if( i === multipleThree){
+     if (i % 3 === 0 && i % 5 === 0){
+        console.log ('FizzBuzz')
+     }
+     else if( i % 3 === 0){
         console.log('Fizz');
     }
-    else if( i === multipleFive){
+    else if (i % 5 === 0) {
         console.log('Buzz');
     }
-
-    if( i === multipleFive && multipleThree){
-        console.log('FizzBuzz');
-    }
+    
+   
 
 
 }
